ceb1643d65a53d324f5f6e2f7c7fded3a0e17fba
Tom5521 <mismarttv321@gmail.com>
2023-11-09 01:16:24 -0300

    Improve release and build scripts

--- scripts/Build.py
+++ scripts/Build.py
@@ -8,31 +8,31 @@ import platform
 import shutil
 import sys
 
+OS = platform.system()
+
 def BuildForWindows():
     if not os.path.exists("builds"):
         os.mkdir("builds")
 
     # Check the platform (Windows or not).
-    if platform.system() != "windows":
+    if OS != "Windows":
         os.environ["GGO_ENABLED"] = "1"
         os.environ["CC"] = "/usr/bin/x86_64-w64-mingw32-gcc"
         os.environ["CXX"] = "/usr/bin/x86_64-w64-mingw32-c++"
         os.environ["GOOS"] = "windows"
 
     # Package the application for Windows using fyne package.
-    os.system("fyne package --os windows --exe builds/EduTrack.exe")
+    os.system("fyne package --os windows --exe builds/EduTrack.exe --release")
 
 
 def BuildForLinux():
-    OS = platform.system()
-
     if not os.path.exists("builds"):
         os.mkdir("builds")
 
     if OS != "Windows":
         os.system("fyne package --os linux --release")
     else:
-        os.system("sudo fyne-cross -os linux")
+        os.system("sudo fyne-cross -os linux -release")
 
     if os.path.exists("EduTrack.tar.xz"):
         shutil.move("EduTrack.tar.xz","builds/EduTrack-linux64.tar.xz")
