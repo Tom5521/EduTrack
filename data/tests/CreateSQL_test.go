d24e441955629e18776108ba5558ba4efd59e2f2
Tom5521 <mismarttv321@gmail.com>
2023-11-21 01:02:37 -0300

    Improve sql creating tests

--- data/tests/CreateSQL_test.go
+++ data/tests/CreateSQL_test.go
@@ -2,14 +2,24 @@ package data_test
 
 import (
 	"EduTrack/data"
-	"fmt"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateSQL(t *testing.T) {
+	assert := assert.New(t)
 	err := data.CreateDatabase()
 	if err != nil {
-		fmt.Println(err)
-		t.Fail()
+		assert.Fail("Error creating new database:", err)
+	}
+	if _, err := os.Stat(data.Config.DatabaseFile); os.IsNotExist(err) {
+		assert.Fail("database File not created!:", err)
+	}
+	if file, err := os.Stat(data.Config.DatabaseFile); os.IsExist(err) {
+		if file.Size() == 0 {
+			assert.Fail("Database filesize is 0!")
+		}
 	}
 }
