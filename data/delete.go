49f36652813699f8712850fbb59ef7a90f165410
Tom5521 <mismarttv321@gmail.com>
2023-11-28 14:25:03 -0300

    fix lll

--- data/delete.go
+++ data/delete.go
@@ -21,7 +21,8 @@ func (d *DBStr) DeleteFrom(table, column string, key int) error {
 	const deleteQuery string = `
 		delete from "%v" where "%v" = %v
 	`
-	// For some reason that I can't understand the db.Exec doesn't accept the arguments correctly, so I'll just use fmt.Sprintf for this.
+	// For some reason that I can't understand the db.Exec doesn't accept the arguments correctly,
+	//so I'll just use fmt.Sprintf for this.
 	_, err = db.Exec(fmt.Sprintf(deleteQuery, table, column, key))
 	if err != nil {
 		log.Println(err)
