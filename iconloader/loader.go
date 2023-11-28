fb37427ae17ed9ffcdd574d436913548a5e74abc
Tom5521 <mismarttv321@gmail.com>
2023-11-28 00:04:09 -0300

    Fix ALL iconloader warnings

--- iconloader/loader.go
+++ iconloader/loader.go
@@ -11,34 +11,37 @@ import (
 )
 
 // Dark Icons
+
 var (
-	DevICON_Dark          fyne.Resource = Dev_Dark
-	DownloadICON_Dark     fyne.Resource = Download_Dark
-	ErrorICON_Dark        fyne.Resource = Error_Dark
-	InstallICON_Dark      fyne.Resource = Install_Dark
-	SaveICON_Dark         fyne.Resource = Save_Dark
-	RestartICON_Dark      fyne.Resource = Restart_Dark
-	InfoICON_Dark         fyne.Resource = Info_Dark
-	UninstallICON_Dark    fyne.Resource = Uninstall_Dark
-	UserTemplateICON_Dark fyne.Resource = TemplateUser_Dark
-	AppICON_Dark          fyne.Resource = App_dark
+	DevICONDark          fyne.Resource = Dev_Dark
+	DownloadICONDark     fyne.Resource = Download_Dark
+	ErrorICONDark        fyne.Resource = Error_Dark
+	InstallICONDark      fyne.Resource = Install_Dark
+	SaveICONDark         fyne.Resource = Save_Dark
+	RestartICONDark      fyne.Resource = Restart_Dark
+	InfoICONDark         fyne.Resource = Info_Dark
+	UninstallICONDark    fyne.Resource = Uninstall_Dark
+	UserTemplateICONDark fyne.Resource = TemplateUser_Dark
+	AppICONDark          fyne.Resource = App_dark
 )
 
 // Light Icons
+
 var (
-	DevICON_Light          fyne.Resource = Dev_Light
-	DownloadICON_Light     fyne.Resource = Download_Light
-	ErrorICON_Light        fyne.Resource = Error_Light
-	InstallICON_Light      fyne.Resource = Install_Light
-	SaveICON_Light         fyne.Resource = Save_Light
-	RestartICON_Light      fyne.Resource = Restart_Light
-	InfoICON_Light         fyne.Resource = Info_Light
-	UninstallICON_Light    fyne.Resource = Uninstall_Light
-	UserTemplateICON_Light fyne.Resource = TemplateUser_Light
-	AppICON_Light          fyne.Resource = App_light
+	DevICONLight          fyne.Resource = Dev_Light
+	DownloadICONLight     fyne.Resource = Download_Light
+	ErrorICONLight        fyne.Resource = Error_Light
+	InstallICONLight      fyne.Resource = Install_Light
+	SaveICONLight         fyne.Resource = Save_Light
+	RestartICONLight      fyne.Resource = Restart_Light
+	InfoICONLight         fyne.Resource = Info_Light
+	UninstallICONLight    fyne.Resource = Uninstall_Light
+	UserTemplateICONLight fyne.Resource = TemplateUser_Light
+	AppICONLight          fyne.Resource = App_light
 )
 
 // Themed Icons
+
 var (
 	DevICON          fyne.Resource
 	DownloadICON     fyne.Resource
@@ -53,6 +56,7 @@ var (
 )
 
 // No-Theme Icons
+
 var (
 	PlaceholderICON fyne.Resource = Placeholder
 )
@@ -60,26 +64,26 @@ var (
 // SetThemeIcons sets themed icons based on the Fyne theme (light or dark).
 func SetThemeIcons(thememode fyne.ThemeVariant) {
 	if thememode == 1 { // Set light mode
-		DevICON = DevICON_Light
-		InstallICON = InstallICON_Light
-		DownloadICON = DownloadICON_Light
-		ErrorICON = ErrorICON_Light
-		SaveICON = SaveICON_Light
-		RestartICON = RestartICON_Light
-		InfoICON = InfoICON_Light
-		UninstallICON = UninstallICON_Light
-		UserTemplateICON = UserTemplateICON_Light
-		AppICON = AppICON_Light
+		DevICON = DevICONLight
+		InstallICON = InstallICONLight
+		DownloadICON = DownloadICONLight
+		ErrorICON = ErrorICONLight
+		SaveICON = SaveICONLight
+		RestartICON = RestartICONLight
+		InfoICON = InfoICONLight
+		UninstallICON = UninstallICONLight
+		UserTemplateICON = UserTemplateICONLight
+		AppICON = AppICONLight
 	} else if thememode == 0 { // Set dark mode
-		DevICON = DevICON_Dark
-		InstallICON = InstallICON_Dark
-		DownloadICON = DownloadICON_Dark
-		ErrorICON = ErrorICON_Dark
-		SaveICON = SaveICON_Dark
-		RestartICON = RestartICON_Dark
-		InfoICON = InfoICON_Dark
-		UninstallICON = UninstallICON_Dark
-		UserTemplateICON = UserTemplateICON_Dark
-		AppICON = AppICON_Dark
+		DevICON = DevICONDark
+		InstallICON = InstallICONDark
+		DownloadICON = DownloadICONDark
+		ErrorICON = ErrorICONDark
+		SaveICON = SaveICONDark
+		RestartICON = RestartICONDark
+		InfoICON = InfoICONDark
+		UninstallICON = UninstallICONDark
+		UserTemplateICON = UserTemplateICONDark
+		AppICON = AppICONDark
 	}
 }
