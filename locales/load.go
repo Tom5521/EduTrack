9bbc92efcfb26182b8afc879688866dc25bad7de
Tom5521 <mismarttv321@gmail.com>
2023-12-13 08:10:07 -0300

    Fix possibly bad language name

--- locales/load.go
+++ locales/load.go
@@ -10,16 +10,17 @@ import (
 //go:embed po
 var PoFiles embed.FS
 
-func read(file string, fs embed.FS) []byte {
-	data, err := fs.ReadFile(file)
+func read(file string) []byte {
+	data, err := PoFiles.ReadFile(file)
 	if err != nil {
 		fmt.Println(err)
+		return read("po/en.po")
 	}
 	return data
 }
 
 func GetPo(lang string) *gotext.Po {
 	po := gotext.NewPo()
-	po.Parse(read("po/"+lang+".po", PoFiles))
+	po.Parse(read("po/" + lang + ".po"))
 	return po
 }
