9fe78e336de4f50faef77977dc00914615315f6c
Tom5521 <mismarttv321@gmail.com>
2023-12-15 15:30:41 -0300

    Fix CreateSQL test

--- tests/data/CreateSQL_test.go
+++ tests/data/CreateSQL_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/Tom5521/EduTrack/pkg/conf"
 	"github.com/Tom5521/EduTrack/pkg/data"
 
 	"github.com/stretchr/testify/assert"
@@ -16,12 +17,12 @@ func TestCreateSQL(t *testing.T) {
 	assert := assert.New(t)
 	require := require.New(t)
 	err := data.CreateDatabase()
-	fmt.Println(data.Config.DatabaseFile)
+	fmt.Println(conf.Config.DatabaseFile)
 	require.NoError(err)
-	if _, err = os.Stat(data.Config.DatabaseFile); os.IsNotExist(err) {
+	if _, err = os.Stat(conf.Config.DatabaseFile); os.IsNotExist(err) {
 		assert.Fail("database File not created!:", err)
 	}
-	file, err := os.Stat(data.Config.DatabaseFile)
+	file, err := os.Stat(conf.Config.DatabaseFile)
 	if os.IsExist(err) {
 		if file.Size() == 0 {
 			assert.Fail("Database filesize is 0!")
